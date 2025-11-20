064ae3ef532b4b51e06111c4ade449d3a299ac7a
Fedulov Danil <danya7777kg@gmail.com>
2025-11-20 21:04:01 +0600

    Полностью выполнил задание

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import './App.css'
+import './App.css';
 import {useState} from "react";
 import Circle from "./components/Circle.tsx";
 
@@ -7,24 +7,37 @@ const App = () => {
 
     const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
+    const newNumbers = (): number[] => {
+      const nums: number[] = [];
+
+      while (nums.length < numbers.length) {
+          const randomNum: number = Math.floor(Math.random() * 32) + 5;
+
+          if (!nums.includes(randomNum)) nums.push(randomNum);
+      }
+
+      return nums.sort((a, b) => a - b);
+    };
+
+    const renderNumbers = () => {
+        setNumbers(newNumbers());
+    };
+
+
     return (
         <>
             <div className="button-wrapper">
-                <button type='button'>New numbers</button>
+                <button type='button' onClick={renderNumbers}>New numbers</button>
             </div>
 
             <div  className="container">
-                {numbers.map((num, index) => (
-                    <Circle key={index}  number={num} />
+                {numbers.map((num: number,) => (
+                    <Circle key={num}  number={num} />
                 ))}
             </div>
         </>
-
-    )
+    );
 
 };
-
-
-
 
 export default App
